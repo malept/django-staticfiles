b31d6a26d8620ee6d6c3ae391f1d16ba9811b3fc
Mark Lee <django+staticfiles@lazymalevolence.com>
2010-11-07 21:55:44 -0800

    collectstatic: refactor to allow for better subclassing

--- staticfiles/management/commands/collectstatic.py
+++ staticfiles/management/commands/collectstatic.py
@@ -32,6 +32,31 @@ class Command(NoArgsCommand):
     )
     help = "Collect static files from apps and other locations in a single location."
 
+    STATUS_COPY = 0
+    STATUS_SKIP = 1
+
+    REASON_ALREADY_COPIED = 1
+    REASON_ALREADY_LINKED = 3
+    REASON_NOT_MODIFIED = 2
+
+    REASONS = {
+        REASON_ALREADY_COPIED: 'already copied earlier',
+        REASON_ALREADY_LINKED: 'already linked earlier',
+        REASON_NOT_MODIFIED: 'not modified',
+    }
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+
+        self.destination_storage = get_storage_class(settings.STORAGE)()
+
+        try:
+            self.destination_storage.path('')
+        except NotImplementedError:
+            self.destination_local = False
+        else:
+            self.destination_local = True
+
     def handle_noargs(self, **options):
         symlink = options['link']
         ignore_patterns = options['ignore_patterns']
@@ -41,14 +66,8 @@ class Command(NoArgsCommand):
         self.copied_files = set()
         self.symlinked_files = set()
         self.unmodified_files = set()
-        self.destination_storage = get_storage_class(settings.STORAGE)()
-
-        try:
-            self.destination_storage.path('')
-        except NotImplementedError:
-            self.destination_local = False
-        else:
-            self.destination_local = True
+        self._verbosity = int(options.get('verbosity', 1))
+        self._dry_run = options['dry_run']
 
         if symlink:
             if sys.platform == 'win32':
@@ -74,10 +93,9 @@ Type 'yes' to continue, or 'no' to cancel: """)
             for source, prefix, storage in finder.list(ignore_patterns):
                 self.copy_file(source, prefix, storage, **options)
 
-        verbosity = int(options.get('verbosity', 1))
         actual_count = len(self.copied_files) + len(self.symlinked_files)
         unmodified_count = len(self.unmodified_files)
-        if verbosity >= 1:
+        if self._verbosity >= 1:
             self.stdout.write("\n%s static file%s %s to '%s'%s.\n"
                               % (actual_count, actual_count != 1 and 's' or '',
                                  symlink and 'symlinked' or 'copied',
@@ -99,56 +117,35 @@ Type 'yes' to continue, or 'no' to cancel: """)
             destination = '/'.join([prefix, source])
         else:
             destination = source
-        symlink = options['link']
-        dry_run = options['dry_run']
-        verbosity = int(options.get('verbosity', 1))
+        self._symlink = options['link']
 
-        if destination in self.copied_files:
-            if verbosity >= 2:
-                self.stdout.write("Skipping '%s' (already copied earlier)\n"
-                                  % destination)
+        status, reason = self.file_status(source_storage, source, destination)
+
+        if status == self.STATUS_SKIP:
+            if self._verbosity >= 2:
+                self.stdout.write('Skipping "%s" (%s)\n' % \
+                                  (destination, self.REASONS[reason]))
+            if reason == self.REASON_NOT_MODIFIED:
+                self.unmodified_files.add(destination)
             return False
 
-        if destination in self.symlinked_files:
-            if verbosity >= 2:
-                self.stdout.write("Skipping '%s' (already linked earlier)\n"
+        if self._dry_run:
+            if self._verbosity >= 2:
+                self.stdout.write("Pretending to delete '%s'\n"
                                   % destination)
-            return False
+        else:
+            if self._verbosity >= 2:
+                self.stdout.write("Deleting '%s'\n" % destination)
+            self.destination_storage.delete(destination)
 
-        if self.destination_storage.exists(destination):
-            try:
-                destination_last_modified = \
-                    self.destination_storage.modified_time(destination)
-            except (OSError, NotImplementedError, AttributeError):
-                # storage doesn't support ``modified_time`` or failed.
-                pass
-            else:
-                destination_is_link = os.path.islink(
-                    self.destination_storage.path(destination))
-                if destination_last_modified == source_last_modified:
-                    if (not symlink and not destination_is_link):
-                        if verbosity >= 2:
-                            self.stdout.write("Skipping '%s' (not modified)\n"
-                                              % destination)
-                        self.unmodified_files.add(destination)
-                        return False
-            if dry_run:
-                if verbosity >= 2:
-                    self.stdout.write("Pretending to delete '%s'\n"
-                                      % destination)
-            else:
-                if verbosity >= 2:
-                    self.stdout.write("Deleting '%s'\n" % destination)
-                self.destination_storage.delete(destination)
-
-        if symlink:
+        if self._symlink:
             destination_path = self.destination_storage.path(destination)
-            if dry_run:
-                if verbosity >= 1:
+            if self._dry_run:
+                if self._verbosity >= 1:
                     self.stdout.write("Pretending to symlink '%s' to '%s'\n"
                                       % (source_path, destination_path))
             else:
-                if verbosity >= 1:
+                if self._verbosity >= 1:
                     self.stdout.write("Symlinking '%s' to '%s'\n"
                                       % (source_path, destination_path))
                 try:
@@ -158,8 +155,8 @@ Type 'yes' to continue, or 'no' to cancel: """)
                 os.symlink(source_path, destination_path)
             self.symlinked_files.add(destination)
         else:
-            if dry_run:
-                if verbosity >= 1:
+            if self._dry_run:
+                if self._verbosity >= 1:
                     self.stdout.write("Pretending to copy '%s' to '%s'\n"
                                       % (source_path, destination))
             else:
@@ -170,14 +167,44 @@ Type 'yes' to continue, or 'no' to cancel: """)
                     except OSError:
                         pass
                     shutil.copy2(source_path, destination_path)
-                    if verbosity >= 1:
+                    if self._verbosity >= 1:
                         self.stdout.write("Copying '%s' to '%s'\n"
                                           % (source_path, destination_path))
                 else:
                     source_file = source_storage.open(source)
                     self.destination_storage.save(destination, source_file)
-                    if verbosity >= 1:
+                    if self._verbosity >= 1:
                         self.stdout.write("Copying %s to %s\n"
                                           % (source_path, destination))
             self.copied_files.add(destination)
         return True
+
+    def file_status(self, source_storage, source, destination):
+        '''Determines the status of the file (one of ``STATUS_COPY`` or
+        ``STATUS_SKIP``), with an optional reason (e.g., for skipping).
+        '''
+        if destination in self.copied_files:
+            return self.STATUS_SKIP, self.REASON_ALREADY_COPIED
+        elif destination in self.symlinked_files:
+            return self.STATUS_SKIP, self.REASON_ALREADY_LINKED
+        elif self.destination_storage.exists(destination):
+            if self.file_has_changed(source_storage, source, destination):
+                destination_is_link = os.path.islink(
+                    self.destination_storage.path(destination))
+                if (not self._symlink and not destination_is_link):
+                    return self.STATUS_SKIP, self.REASON_NOT_MODIFIED
+        return self.STATUS_COPY, None
+
+    def file_has_changed(self, source_storage, source, destination):
+        try:
+            source_last_modified = source_storage.modified_time(source)
+        except (OSError, NotImplementedError, AttributeError):
+            source_last_modified = 0
+        try:
+            destination_last_modified = \
+                self.destination_storage.modified_time(destination)
+        except (OSError, NotImplementedError, AttributeError):
+            # storage doesn't support ``modified_time`` or failed, assume True
+            return True
+        else:
+            return source_last_modified > destination_last_modified
